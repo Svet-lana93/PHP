55ad80530a90a0a544a32d82e5d587fd71049f23
Svetlana Shmidt <svetashmidt93@gmail.com>
2022-04-28 22:34:28 +0300

    new commit

---
+++ server/public/index.php
@@ -0,0 +1,9 @@
+<?php
+echo 'Test docker';
+print_r($_POST);
+?>
+
+<form method="post" action="index.php">
+    <input type="text" name="firstname">
+    <button type="submit">Send</button>
+</form>
