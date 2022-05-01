6cd3e4d6e668e1b1f879d8c724fba8514b15efa0
Svetlana Shmidt <svetashmidt93@gmail.com>
2022-05-01 23:52:33 +0300

    Redirect header has been added

--- server/public/index.php
+++ server/public/index.php
@@ -1,18 +1,4 @@
 <?php
-//echo 'Test docker';
-//print_r($_POST);
-?>
-<h2>Review Form</h2>
-
-<form method="post" action="index.php">
-    <label for="user">Your name</label><br>
-    <input type="text" name="user"><br>
-    <label for="review">Comments</label><br>
-    <textarea name="review"></textarea><br>
-    <button type="submit">Submit</button>
-</form>
-
-<?php
 $servername = 'a_level_nix_mysql';
 $database = 'a_level_nix_mysql';
 $username = 'root';
@@ -29,11 +15,26 @@ if ($user !== '' && $review !== '') {
     $sql = 'INSERT INTO `reviews` (`user`, `review`) 
                 VALUES ("' . mysqli_real_escape_string($conn, $user) . '","' . mysqli_real_escape_string($conn, $review) . '")';
     if (mysqli_query($conn, $sql)) {
-        echo 'Data added successfully';
+//        echo 'Data added successfully';
+        header('Location: /');
+        mysqli_close($conn);
+        exit;
     } else {
         echo 'Error: ' . mysqli_error($conn);
     }
-} else {
-    echo 'No data';
 }
+
+?>
+
+<h2>Review Form</h2>
+
+<form method="post" action="index.php">
+    <label for="user">Your name</label><br>
+    <input type="text" name="user"><br>
+    <label for="review">Comments</label><br>
+    <textarea name="review"></textarea><br>
+    <button type="submit">Submit</button>
+</form>
+
+<?php
 mysqli_close($conn);
